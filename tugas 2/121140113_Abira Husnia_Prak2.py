fe78805204f1d8e39ce4f24265c5b913bdecf984
abirahusnia <105179110+abirahusnia@users.noreply.github.com>
2023-03-06 20:04:51 +0700

    Update 121140113_Abira Husnia_Prak2.py

--- tugas 2/121140113_Abira Husnia_Prak2.py
+++ tugas 2/121140113_Abira Husnia_Prak2.py
@@ -1,18 +1,24 @@
-#tugas prak 1 RB
+#tugas prak 2 RB
 #121140113 Abira Husnia
 
+#class
 class Data:
+    #konstruktor
     def __init__(self, nama, nim, kelas, sks, semester):
         self.nama = nama
         self.nim = nim
         self.kelas = kelas
         self.sks = sks
         self.semester = semester
-        
+
+    #method
+    def data_siakad(self):
+        print("Nama             : ", self.nama)
+        print("NIM              : ", self.nim)
+        print("Kelas Siakad PBO : ", self.kelas)
+        print("Jumlah SKS       : ", self.sks)
+        print("Semester         : ", self.semester)
+
+#objek        
 d1 = Data("Abira Husnia", "121140113", "RB", "22", "4")
-        
-print("Nama             : ", d1.nama)
-print("NIM              : ", d1.nim)
-print("Kelas Siakad PBO : ", d1.kelas)
-print("Jumlah SKS       : ", d1.sks)
-print("Semester         : ", d1.semester)
+d1.data_siakad()
